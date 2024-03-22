ec45e39cbf84efebd86b57d9ea3a13959d7f2c0d
SeddonShen <283481855@qq.com>
2024-03-22 01:45:11 +0000

    feat: change theme index

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,5 @@
-// import mediumZoom from "medium-zoom";
-// import "medium-zoom/dist/style.css";
-import ArcoVue from '@arco-design/web-vue'
-import '@arco-design/web-vue/dist/arco.css'
+import mediumZoom from 'medium-zoom'
+import 'medium-zoom/dist/style.css'
 import type { Theme } from 'vitepress'
 import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
@@ -9,5 +7,31 @@ import { h } from 'vue'
 import { onMounted, watch, nextTick } from 'vue'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  },
+  setup() {
+    //  添加以下代码 --》
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('.content-container p img', {
+        background: 'var(--vp-c-bg)',
+        container: document.body
+      })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+    //  《--- 结束
+  }
 } satisfies Theme
